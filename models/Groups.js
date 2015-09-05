9722ed300d51537f206100520b3cd53b2bc961b3
Malith Senaweera <malithsenaweera@gmail.com>
2015-09-05 10:15:25 +0530

    Initial commit

---
+++ models/Groups.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+
+
+/**
+ * Group Schema
+ */
+
+var GroupSchema = new Schema({
+  name : {type : String }
+  }
+);
+
+mongoose.model('Groups', GroupSchema);
+
---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+function createTracker() {
+  $('.dup-warn').hide();
+
+  dname = $('#devicename').val();
+  var dmac = '';
+  $( ".devicemac" ).find('input').each(function(i,o){
+    dmac = dmac + $(this).val().toUpperCase();
+  });
+
+  // Check whether MAC already exists in the db if not create a new Tracker
+  if (validateMAC() === true) {
+    $.get('/api/tracker/findmac/' + dmac).done(function(res){
+      if (res.length === 0) {
+        $.post('/api/tracker/create', { name: dname, device: {mac: dmac} });
+      } else {
+        $('.dup-warn').show();}
+    });
+  }
+}
+
+function validateMAC() {
+  var regex = /^[0-9A-Fa-f]{2}$/;
+  var valid = true;
+
+  $( ".devicemac" ).find('input').each(function(i,o){
+    var validmac = regex.test($(this).val());
+    if (!validmac){
+      $(this).addClass("incorrect-mac");
+      valid = valid && false;
+    } else {
+      $(this).removeClass("incorrect-mac");
+    }
+  });
+  return valid;
+}
+
+function initialize() {
+  var mylatlng = new google.maps.LatLng(6.9344, 79.8428); // Should find a way to automatically set the focus
+  var mapProp = {
+    center: mylatlng,
+    zoom:7,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map($("#googleMap")[0], mapProp);
+
+  $.get('api/tracker/locations').done(function(res){
+    for(var i=0; i<res.length; i++){
+      setMarker(res[i], map);
+    }
+  });
+
+}
+
+function setMarker(res, map){
+  getNameFromId(res._id, function(name){
+    var latLng = new google.maps.LatLng(res.path[0], res.path[1]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: name
+    });
+  });
+}
+
+function getNameFromId(id, cb){
+  $.get('api/tracker/'+id).done(function(res){
+    cb(res[0].name);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
